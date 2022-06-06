5de03463924472c9aa69576f9ed3036e6d7ce3fa
dfpinto <dirley.figueredo@gmail.com>
2022-06-06 00:02:21 -0300

    Add PieChartCard component

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -3,6 +3,7 @@ import Header from './components/header';
 import './App.css';
 import SalesByDate from './components/sales-by-date';
 import SalesSummary from './components/sales-summary';
+import PieChartCard from './components/pie-chart-card';
 
 function App() {
   return (
@@ -13,6 +14,16 @@ function App() {
         <SalesByDate />
         <div className="sales-overview-container">
           <SalesSummary />
+          <PieChartCard
+            labels={['Uberlândia', 'Araguari', 'Uberaba']}
+            name="Lojas"
+            series={[40, 30, 30]}
+          />
+          <PieChartCard
+            labels={['Crêdito', 'Débito', 'Dinheiro']}
+            name="Pagamento"
+            series={[20, 50, 30]}
+          />
         </div>
       </div>
     </>
---
+++ frontweb/src/components/pie-chart-card/index.tsx
@@ -0,0 +1,25 @@
+import ReactApexChart from 'react-apexcharts';
+import { buildPieChartConfig } from './helpers';
+import './styles.css';
+
+type Props = {
+  labels: string[];
+  name: string;
+  series: number[];
+};
+
+function PieChartCard({ labels, name, series }: Props) {
+  return (
+    <header className="pie-chart-card base-card">
+      <ReactApexChart
+        options={buildPieChartConfig(labels, name)}
+        type="donut"
+        width={400}
+        height={400}
+        series={series}
+      />
+    </header>
+  );
+}
+
+export default PieChartCard;
